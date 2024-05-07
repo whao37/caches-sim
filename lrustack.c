94a825ff4e2a2ad3a6f93d63e364e18b0db738d4
William Hao <86330766+whao37@users.noreply.github.com>
2024-05-07 00:13:37 -0400

    Update lrustack.c

--- lrustack.c
+++ lrustack.c
@@ -6,24 +6,6 @@
 #include "lrustack.h"
 
 /**
- * This file contains some starter code to get you started on your LRU implementation. 
- * You are free to implement it however you see fit. You can design it to emulate how this
- * would be implemented in hardware, or design a purely software stack. 
- * 
- * We have broken down the LRU stack's
- * job/interface into two parts:
- *  - get LRU: gets the current index of the LRU block
- *  - set MRU: sets a certain block's index as the MRU. 
- * If you implement it using these suggestions, you will be able to test your LRU implementation
- * using lrustacktest.c
- * 
- * NOTES: 
- *      - You are not required to use this LRU interface. Feel free to design it from scratch if 
- *      that is better for you.
- *      - This will not behave like your traditional LIFO stack  
- */
-
-/**
  * Function to initialize an LRU stack for a cache set with a given <size>. This function
  * creates the LRU stack. 
  * 
@@ -37,15 +19,6 @@ lru_stack_t* init_lru_stack(int size) {
 	stack->size = size;
     stack->priorityBits = (int*)malloc(size*sizeof(int));
     
-    ////////////////////////////////////////////////////////////////////
-    //  TODO: Write any other code needed to initialize your LRU Stack. 
-    //  Essentially write any initializations needed for anything you
-    //  added to lru_stack_t.
-    ////////////////////////////////////////////////////////////////////
-
-    ////////////////////////////////////////////////////////////////////
-    //  End of your code   
-    ////////////////////////////////////////////////////////////////////
 
 	return stack;
 }
@@ -58,10 +31,7 @@ lru_stack_t* init_lru_stack(int size) {
  * @return the index of the LRU cache block.
  */
 int lru_stack_get_lru(lru_stack_t* stack) {
-    ////////////////////////////////////////////////////////////////////
-    //  TODO: Write code to get the index of the LRU block from the LRU 
-    //  Stack. 
-    ////////////////////////////////////////////////////////////////////
+
     int minimum_index = 0;
     for (int i = 0; i < stack->size; i++) {
         if (stack->priorityBits[i] < stack->priorityBits[minimum_index]) {
@@ -83,10 +53,7 @@ int lru_stack_get_lru(lru_stack_t* stack) {
  * @param n the index to promote to MRU.  
  */
 void lru_stack_set_mru(lru_stack_t* stack, int n) {
-	////////////////////////////////////////////////////////////////////
-    //  TODO: Write code to set the passed in block index  as the MRU 
-    //  element in the LRU Stack. 
-    ////////////////////////////////////////////////////////////////////
+
     int maximumPriority = stack->priorityBits[0];
     for(int i = 1; i < stack->size; i++){
         if (stack->priorityBits[i] > maximumPriority) {
@@ -94,9 +61,7 @@ void lru_stack_set_mru(lru_stack_t* stack, int n) {
         }
     }
     stack->priorityBits[n] = maximumPriority + 1;
-    ////////////////////////////////////////////////////////////////////
-    //  End of your code   
-    ////////////////////////////////////////////////////////////////////
+
 }
 
 /**
@@ -105,14 +70,6 @@ void lru_stack_set_mru(lru_stack_t* stack, int n) {
  * @param stack the stack to free
  */
 void lru_stack_cleanup(lru_stack_t* stack) {
-    ////////////////////////////////////////////////////////////////////
-    //  TODO: Write any code if you need to do additional heap allocation
-    //  cleanup
-    ////////////////////////////////////////////////////////////////////
-
-    ////////////////////////////////////////////////////////////////////
-    //  End of your code   
-    ////////////////////////////////////////////////////////////////////
 
     free(stack);        // Free the stack struct we malloc'd
 }
